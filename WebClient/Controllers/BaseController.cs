9755444aa05ec73da3ad3e89ce61c002ce7bcb5f
trngchnii <ni.trnh59@gmail.com>
2024-10-29 22:08:56 +0700

    call api edit profile influencer

---
+++ WebClient/Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using WebClient.Areas.Admin.Data;
+
+namespace WebClient.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected readonly HttpClient _httpClient;
+        protected readonly string CategoryAPIURL;
+        protected readonly string BrandAPIURL;
+        protected readonly string InfluencerAPIURL;
+        public BaseController(IConfiguration configuration)
+        {
+            _httpClient = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            _httpClient.DefaultRequestHeaders.Accept.Add(contentType);
+
+            var apiSettings = new ApiSettings();
+            configuration.GetSection("ApiUrls").Bind(apiSettings);
+
+            CategoryAPIURL = apiSettings.CategoryAPIURL;
+            BrandAPIURL = apiSettings.BrandAPIURL;
+            InfluencerAPIURL = apiSettings.InfluencerAPIURL;
+        }
+
+    }
+}
--- WebClient/Controllers/InfluencerController.cs
+++ WebClient/Controllers/InfluencerController.cs
@@ -1,20 +1,81 @@
+﻿using api.DTOs;
+using api.Models;
+using api.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text;
+using WebClient.Models;
 
 namespace WebClient.Controllers
 {
-    public class InfluencerController : Controller
+    public class InfluencerController : BaseController
     {
-        private readonly ILogger<InfluencerController> _logger;
-
-        public InfluencerController(ILogger<InfluencerController> logger)
+        public class InfluencerResponse
         {
-            _logger = logger;
+            [JsonProperty("value")]
+            public List<Influencer> Value { get; set; }
+        }
+        public InfluencerController(IConfiguration configuration) : base(configuration)
+        {
         }
 
-
-        public IActionResult EditProfile()
+        public async Task<IActionResult> EditProfile()
         {
-            return View();
+            // Lấy InfluencerId từ cookie
+            var influencerId = HttpContext.Request.Cookies["InfluencerId"];
+
+            // Kiểm tra nếu InfluencerId tồn tại trong cookie
+            if (string.IsNullOrEmpty(influencerId))
+            {
+                return View("Error",new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+
+            // Gọi API để lấy thông tin của Influencer dựa trên InfluencerId từ cookie
+            string str = InfluencerAPIURL;
+            HttpResponseMessage res = await _httpClient.GetAsync($"{str}{influencerId}");
+
+            if (!res.IsSuccessStatusCode)
+            {
+                return View("Error",new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+
+            string rData = await res.Content.ReadAsStringAsync();
+            var response = JsonConvert.DeserializeObject<UpdateInfluencerRequestDto>(rData);
+
+            return View(response);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditProfile(UpdateInfluencerRequestDto influencer)
+        {
+            if (ModelState.IsValid)
+            {
+                // Lấy InfluencerId từ cookie
+                var influencerId = HttpContext.Request.Cookies["InfluencerId"];
+
+                if (string.IsNullOrEmpty(influencerId))
+                {
+                    return View("Error",new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+                }
+
+                var json = JsonConvert.SerializeObject(influencer);
+                var content = new StringContent(json,Encoding.UTF8,"application/json");
+
+                string str = InfluencerAPIURL;
+                HttpResponseMessage res = await _httpClient.PutAsync($"{str}{influencerId}",content);
+
+                if (res.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index)); // Điều hướng đến một action phù hợp sau khi chỉnh sửa thành công
+                }
+
+                ModelState.AddModelError(string.Empty,"Server Error");
+            }
+
+            return View(influencer);
+        }
+
     }
 }
